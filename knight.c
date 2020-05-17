b5e9eaf02208d159526aeb45c8ce150813a77efd
TanchukVlad <tanchuk.vladislav@gmail.com>
2020-05-17 20:02:42 +0300

    Init

---
+++ knight.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int steps;  
+	int n, k;  
+	n = atoi (argv[1]);  
+	k = atoi (argv[2]);	 
+	
+	printf("%d\n", steps);	
+
+	return 0;
+}
